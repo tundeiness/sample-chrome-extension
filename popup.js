7056b07e048e93740f0f59fc17ff1af18f6feb01
tundeiness <tundeiness@yahoo.co.uk>
2020-10-29 15:21:28 +0100

    add options.html and options.js

--- popup.js
+++ popup.js
@@ -4,3 +4,12 @@ let switchColor = document.getElementById('switchColor');
     switchColor.style.backgroundColor = data.color;
     switchColor.setAttribute('value', data.color);
   });
+
+  switchColor.onclick = function(element) {
+    let color = element.target.value;
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.executeScript(
+          tabs[0].id,
+          {code: 'document.body.style.backgroundColor = "' + color + '";'});
+    });
+  };
